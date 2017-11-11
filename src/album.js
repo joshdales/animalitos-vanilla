df85d65ca9f8fe4840fee9e7c9c2ee92c7c1e1d0
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-11 15:09:42 -0500

    get unsplash data

--- src/album.js
+++ src/album.js
@@ -7,5 +7,12 @@ class Album {
 
   show(animal) {
     this.section.classList.add('active');
+    
+    const client = 'a1aff3b188b6ffd926e3c12232423dcf29f199ea31f5ef7fd561d1f0d998dcb5'
+
+    fetch(`https://api.unsplash.com/search/photos?client_id=${client}&query=${animal.name}`).then((respose) => {return respose.json();
+    }).then(data => {
+      console.log(data);
+    })
   };
 }
