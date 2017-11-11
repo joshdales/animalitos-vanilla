ef0830eabbe20d8b41eddbc028b4330c5a703f18
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-11 14:20:14 -0500

    Add caption to animals

--- src/main.js
+++ src/main.js
@@ -11,6 +11,14 @@ const createAnimalFigure = (animal) => {
   background.style.backgroundImage = `url('${animal.image}')`;
   figure.appendChild(background)
 
+  const caption = document.createElement('figcaption');
+  caption.style.backgroundColour = animal.bgColour;
+  caption.innerHTML = `
+    <h2>${animal.name}</h2>
+    <img src="${animal.icon}" alt="${animal.name}" class='animal-icon'/>
+  `;
+  figure.appendChild(caption);
+
   return figure;
 }
 
