e37d03a24cc43da2c63d7fed790c7d8f558db700
JOSH DALES <DALES.JOSH@GMAILCOM>
2018-01-10 09:17:28 -0500

    Remove obsolete Unplash key

--- src/album.js
+++ src/album.js
@@ -8,7 +8,7 @@ class Album {
   show(animal) {
     this.section.classList.add('active');
 
-    const client = 'a1aff3b188b6ffd926e3c12232423dcf29f199ea31f5ef7fd561d1f0d998dcb5'
+    const client = // Key goes here
 
     fetch(`https://api.unsplash.com/search/photos?client_id=${client}&query=${animal.name}`).then((respose) => {return respose.json();
     }).then(data => {
